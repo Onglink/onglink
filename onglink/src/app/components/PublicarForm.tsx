65e78814eb221622edfecc628a5a5155a37f8d65
ViniReis2811 <vinireis2811@gmail.com>
2025-10-23 21:10:15 -0300

    correção p/ vercel

--- onglink/src/app/components/PublicarForm.tsx
+++ onglink/src/app/components/PublicarForm.tsx
@@ -8,7 +8,7 @@ import { criarPublicacao } from '@/app/feed/publicacaoService';
 import { uploadImagemParaCloudinary } from '@/app/components/uploadImage/uploadImage';
 
 interface PublicarFormProps {
-  onPublish: (post: unknown) => void;
+  onPublish: (post: any) => void;
 }
 
 export default function PublicarForm({ onPublish }: PublicarFormProps) {
