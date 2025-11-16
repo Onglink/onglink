b1c2ed9b39a5a41d208c305c5c94c11ba6d007ef
RafaelS-Costa <rafaelsartoricosta@outlook.com>
2025-11-16 15:51:05 -0300

    ajuste no cadastro simples, integrado ao backend e funcionando

--- onglink/src/app/services/usuarioService.js
+++ onglink/src/app/services/usuarioService.js
@@ -4,7 +4,7 @@ import api from './api';
 const usuarioSevice = {
     listarUsuario: async () => (await api.get('/usuarios')).data,
     buscarPorId: async (id) => (await api.get(`/usuarios/${id}`)).data,
-    cadastrarUsuario: async (data) => (await api.post('/usuarios',data)).data,
+    cadastrarUsuario: async (data) => (await api.post('/usuarios/cadastro',data)).data,
     atualizarUsuario: async(id, data) => (await api.put(`/usuarios/${id}`,data)).data,
     deletarUsuario: async(id) => (await api.delete(`/usuarios/${id}`)).data,
     loginUsuario: async(data) => (await api.post('/usuarios/login',data)).data,
