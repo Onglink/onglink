f8f592fe3eb7291ad1eb88a12d42f22137602440
RafaelS-Costa <rafaelsartoricosta@outlook.com>
2025-11-20 00:37:16 -0300

    alteração no publicação service.

--- onglink/src/app/services/publicacaoService.js
+++ onglink/src/app/services/publicacaoService.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const publicacaoSevice = {
     listarPublicacao: async () => (await api.get('/publicacoes')).data,
     buscarPublicacao: async (id) => (await api.get(`/publicacoes/${id}`)).data,
-    cadastrarPublicacao: async (data) => (await api.post('/publicacoes',data)).data,
+    cadastrarPublicacao: async (formData) => (await api.post('/publicacoes',formData)).data,
     atualizarPublicacao: async(id,data) => (await api.put(`/publicacoes/${id}`,data)).data,
     deletarPublicacao: async(id) => (await api.delete(`/publicacoes/${id}`)).data,
 };
