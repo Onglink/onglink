4133bf7645007eb390ea15f726add5ee8cb0b07b
RafaelS-Costa <rafaelsartoricosta@outlook.com>
2025-11-19 22:07:11 -0300

    alteração no service api.js para rodar o publicações

--- onglink/src/app/services/api.js
+++ onglink/src/app/services/api.js
@@ -6,5 +6,22 @@ const api = axios.create({
     'x-api-key': process.env.NEXT_PUBLIC_API_KEY || 'fcf87fe4-7793-477d-b765-620c5cdbcf4f',
   }
 });
+api.interceptors.request.use(
+    (config) => {
+        
+        // 'authToken' deve ser a mesma chave que você usou no login!
+        const token = typeof window !== 'undefined' ? localStorage.getItem('authToken') : null;
+
+        // Se tiver token, adiciona no cabeçalho Authorization
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default api;
